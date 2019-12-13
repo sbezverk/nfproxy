135eae4b457c1187272ccb153f41647f67ef29fa
Serguei Bezverkhi <sbezverk@cisco.com>
2019-12-13 14:09:19 -0500

    refactor endpoint rule programming logic
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"sync"
+	"time"
 
 	utilnftables "github.com/google/nftables"
 	"github.com/sbezverk/nfproxy/pkg/nftables"
@@ -11,6 +12,10 @@ import (
 	"k8s.io/klog"
 	utilproxy "k8s.io/kubernetes/pkg/proxy/util"
 	utilnet "k8s.io/utils/net"
+)
+
+var (
+	nfRuleRetryInterval = time.Second * 30
 )
 
 // Proxy defines interface
@@ -35,13 +40,12 @@ func (p *proxy) AddService(svc *v1.Service) {
 		servicePort := &svc.Spec.Ports[i]
 		svcPortName := ServicePortName{NamespacedName: svcName, Port: servicePort.Name, Protocol: servicePort.Protocol}
 		baseSvcInfo := newBaseServiceInfo(servicePort, svc)
-		//		klog.Infof("AddService service port name: %s   new service Info: %+v", svcPortName, newServiceInfo(servicePort, svc, baseSvcInfo))
 		p.mu.Lock()
 		p.serviceMap[svcPortName] = newServiceInfo(servicePort, svc, baseSvcInfo)
 		ep, ok := p.endpointsMap[svcPortName]
 		p.mu.Unlock()
 		if !ok {
-			klog.Infof("service: %s does not have corresponding endpoints %s.", svcPortName)
+			klog.Infof("service: %s does not have corresponding endpoints.", svcPortName)
 		} else {
 			klog.Infof("service: %s has corresponding endpoints %+v.", svcPortName, ep)
 		}
@@ -54,6 +58,7 @@ func (p *proxy) DeleteService(svc *v1.Service) {
 func (p *proxy) UpdateService(svcOld, svcNew *v1.Service) {
 
 }
+
 func (p *proxy) AddEndpoints(ep *v1.Endpoints) {
 	for i := range ep.Subsets {
 		ss := &ep.Subsets[i]
@@ -74,6 +79,18 @@ func (p *proxy) AddEndpoints(ep *v1.Endpoints) {
 					klog.Warningf("ignoring invalid endpoint port %s with empty host", port.Name)
 					continue
 				}
+				// Check if combination of Protocol, ip addres and port already exists in endpoint ongoing programming map
+				// it should not happen during Add operation and indicate a software issue, warning is logged to alert.
+				if epStopCh, ok := p.epProgMap[epKey{port.Protocol, addr.IP, port.Port}]; ok {
+					klog.Warningf("nfproxy: Attempting to add already known endpoint for protocol: %s ip address: %s port: %d", port.Protocol, addr.IP, port.Port)
+					// Shutting down EP programmer go routine for port.Protocol, addr.IP, port.Port
+					epStopCh <- struct{}{}
+					// Deleting entry from epProgMap
+					p.epProgMapLock.Lock()
+					delete(p.epProgMap, epKey{port.Protocol, addr.IP, port.Port})
+					p.epProgMapLock.Unlock()
+				}
+
 				isLocal := addr.NodeName != nil && *addr.NodeName == p.hostname
 				var ipFamily v1.IPFamily
 				var ipTableFamily utilnftables.TableFamily
@@ -91,33 +108,54 @@ func (p *proxy) AddEndpoints(ep *v1.Endpoints) {
 					Rule:      make(map[utilnftables.TableFamily]nftables.EPRule),
 				}
 				cn := servicePortEndpointChainName(svcPortName.String(), string(port.Protocol), baseEndpointInfo.Endpoint)
-				ruleID, err := nftables.ProgramNewEndpoint(p.nfti, ipTableFamily, cn, addr.IP, port.Protocol, port.Port)
-				if err != nil {
-					// If programming of rule failed, then do not add endpoint into the map
-					// early return and hope to add it during next controller's resync
-					klog.Errorf("nfproxy failed to program new endpoint for %s", svcPortName)
-					return
-				}
 				// Initializing ip table family depending on endpoint's family ipv4 or ipv6
-				baseEndpointInfo.epnft.Rule[ipTableFamily] = nftables.EPRule{
-					Chain:  cn,
-					RuleID: ruleID,
+				epRule := nftables.EPRule{
+					Chain: cn,
+					// RuleID 0 is indicator that the nftables rule has not been yet programmed, once it is programed
+					// RuleID will be updated to real value.
+					RuleID: 0,
 				}
-				// klog.Infof("AddEndpoint: service port name: %s  new endpoint Info: %+v", svcPortName, newEndpointInfo(baseEndpointInfo, port.Protocol))
-
+				baseEndpointInfo.epnft.Rule[ipTableFamily] = epRule
 				p.mu.Lock()
 				p.endpointsMap[svcPortName] = append(p.endpointsMap[svcPortName], newEndpointInfo(baseEndpointInfo, port.Protocol))
-				svc, ok := p.serviceMap[svcPortName]
 				p.mu.Unlock()
-				if !ok {
-					klog.Infof("endpoint: %s does not have corresponding service.", svcPortName)
-				} else {
-					klog.Infof("endpoint: %s has corresponding service %s.", svcPortName, svc.String())
-				}
+				// Starting go routine which will attempt to program nftables rules, on success it will update RuleID,
+				// remove entry from epProgMap for key port.Protocol, addr.IP, port.Port and exit
+				p.epProgMapLock.Lock()
+				p.epProgMap[epKey{port.Protocol, addr.IP, port.Port}] = make(chan struct{})
+				p.epProgMapLock.Unlock()
+				go p.programEndpoint(&epRule, ipTableFamily, cn, addr.IP, port.Protocol, port.Port)
+
 			}
 			klog.V(3).Infof("Setting endpoints for %q to %+v", svcPortName, formatEndpointsList(p.endpointsMap[svcPortName]))
 		}
 	}
+}
+
+func (p *proxy) programEndpoint(epRule *nftables.EPRule, ipTableFamily utilnftables.TableFamily, cn string, ipaddr string, proto v1.Protocol, port int32) {
+	stopCh := p.epProgMap[epKey{proto, ipaddr, port}]
+	retrier := time.NewTicker(nfRuleRetryInterval)
+	var ruleID uint64
+	var err error
+	for {
+		ruleID, err = nftables.ProgramNewEndpoint(p.nfti, ipTableFamily, cn, ipaddr, proto, port)
+		if err == nil {
+			break
+		}
+		select {
+		case <-stopCh:
+			retrier.Stop()
+			return
+		case <-retrier.C:
+		}
+	}
+	// Programming nftables rule has succeeded, updating RuleID and removing entry from epProgMap
+	p.mu.Lock()
+	epRule.RuleID = ruleID
+	p.mu.Unlock()
+	p.epProgMapLock.Lock()
+	delete(p.epProgMap, epKey{proto, ipaddr, port})
+	p.epProgMapLock.Unlock()
 }
 
 func (p *proxy) DeleteEndpoints(ep *v1.Endpoints) {
@@ -128,12 +166,20 @@ func (p *proxy) UpdateEndpoints(epOld, epNew *v1.Endpoints) {
 }
 
 type proxy struct {
-	hostname     string
-	nfti         *nftables.NFTInterface
-	mu           sync.Mutex // protects the following fields
-	serviceMap   ServiceMap
-	endpointsMap EndpointsMap
-	portsMap     map[utilproxy.LocalPort]utilproxy.Closeable
+	hostname      string
+	nfti          *nftables.NFTInterface
+	mu            sync.Mutex // protects the following fields
+	serviceMap    ServiceMap
+	endpointsMap  EndpointsMap
+	portsMap      map[utilproxy.LocalPort]utilproxy.Closeable
+	epProgMapLock sync.Mutex
+	epProgMap     map[epKey]chan struct{}
+}
+
+type epKey struct {
+	proto  v1.Protocol
+	ipaddr string
+	port   int32
 }
 
 // NewProxy return a new instance of nfproxy
@@ -144,5 +190,6 @@ func NewProxy(nfti *nftables.NFTInterface, hostname string, recorder record.Even
 		portsMap:     make(map[utilproxy.LocalPort]utilproxy.Closeable),
 		serviceMap:   make(ServiceMap),
 		endpointsMap: make(EndpointsMap),
+		epProgMap:    make(map[epKey]chan struct{}),
 	}
 }
